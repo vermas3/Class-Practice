6337491a5ef80990e5cccdc42061229fa1fe6749
vermas3 <vermas3@SOB-B2113-19>
2017-10-03 18:20:18 -0400

    Count Letters

--- CountLetters/CountLetters.cpp
+++ CountLetters/CountLetters.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int countletters(char myphr[100]);
 
 
-int main()
-{
-    return 0;
+int main() {
+	char myphrase[100];
+	cout << "Enter phrase" << endl;
+	gets_s(myphrase);
+
+	int ct =countletters(myphrase);
+	cout << " you have " << ct << " letters " << endl;
+	return 0;
 }
+
+
+int countletters(char myphr[100]) 
+{
+		int i;
+		int count = 0;
+		for ( i = 0; i < strlen(myphr); i++)
+		{
+			if (myphr[i]!=' ' && myphr[i]!=',' && myphr[i]!='!')
+			{
+				count += 1;
+			}
+		}
+		return count;
+	}
+    
 
